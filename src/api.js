9e3eb2aec0f531ba57201fb920f66ff9d96f866f
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-11-20 01:04:58 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,7 +8,34 @@ app.use(cors())
 
 router.get('/',(req,res)=>{
     res.json({
-        message: 'Hello World'
+        map1:
+            {
+                images : [
+                    {
+                        url: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
+                    }
+                ],
+                iframes : [
+                    {
+                    url: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
+                    }
+                ],
+                label: [
+                    {
+                        text:'MAP 1'
+                    }
+                ],
+                space: [
+                    {
+                    showCode: true,
+                    showDemo: true,
+                    showTeam: true,
+                    code: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
+                    demo: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
+                    team: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
+                    }
+                ],
+              }
     })
 });
 
