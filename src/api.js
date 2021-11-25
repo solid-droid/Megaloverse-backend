272ca36cc853c97efe4db409705c0b60ede063a2
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-11-25 18:02:25 +0530

    backend update + DB connect

--- src/api.js
+++ src/api.js
@@ -2,54 +2,79 @@ const express = require('express')
 const serverless = require('serverless-http')
 const cors = require('cors')
 
+const mongoose = require('mongoose')
+const schema = require('./dolbyMap')
+
 const app = express()
 const router = express.Router();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
-router.get('/',(req,res)=>{
-    res.json({
-        map1:
-            {
-                images : [
-                    {
-                        url: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
-                    }
-                ],
-                iframes : [
-                    {
-                    url: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
-                    }
-                ],
-                label: [
-                    {
-                        text:'MAP 1'
-                    }
-                ],
-                space: [
-                    {
-                    showCode: true,
-                    showDemo: true,
-                    showTeam: true,
-                    code: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
-                    demo: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
-                    team: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
-                    }
-                ],
-              },
-        map2:
-            {
-                images : [],
-                iframes : [
-                            {
-                            url: 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1'
-                            }
-                        ],
-                label: [],
-                space: [],
-            }
-    })
+router.get('/', async (req,res)=>{
+    try{
+        const products = await schema.find();
+        res.status(200).json({success: true, data: products})
+    } catch(err)
+    {
+        res.status(409).json({success: false, data: [], error: err})
+        console.log(err)
+    }
 });
 
+router.get('/:map', async (req,res)=>{
+    try{
+        const {map} = req.params
+        const product = await schema.find({map});
+        res.status(200).json({success: true, data: product})
+    } catch(err)
+    {
+        res.status(409).json({success: false, data: [], error: err})
+        console.log(err)
+    }
+});
+
+router.post('/createMap', async (req,res)=>{
+    try{
+        const {map, data} = req.body
+        console.log(req.body);
+        const products = await schema.find();
+     if(!products.find(product => product.map === map)){
+        const product = new schema({ map, data });
+        const saveProduct = await product.save();
+        res.status(200).json({success: true, exists: false, data: saveProduct})
+     }else{
+         res.status(200).json({success: true, exists : true})
+     }
+    } catch(err)
+    {
+        res.status(409).json({success: false, data: [], error: err})
+        console.log(err)
+    }
+});
+
+
+
+router.post('/updateMap', async (req,res)=>{
+    try{
+        const {map, data} = req.body
+        const products = await schema.find();
+        let product = products.find(product => product.map === map);
+     if(product){
+        product.data = data;
+        const saveProduct = await product.save();
+        res.status(200).json({success: true, notFound: false, data: saveProduct})
+     }else{
+         res.status(200).json({success: true, notFound : true})
+     }
+    } catch(err)
+    {
+        res.status(409).json({success: false, data: [], error: err})
+        console.log(err)
+    }
+});
+
+mongoose.connect('mongodb+srv://nikhil:qwerty1234@cluster0.87dy8.mongodb.net/DolbyMaps?retryWrites=true&w=majority', () => console.log('DB connected'))
 
 app.use('/.netlify/functions/api',router);
 module.exports.handler = serverless(app)
---
+++ src/dolbyMap.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const dolbyMapSchema = new mongoose.Schema({
+    map : {
+        type : String,
+        required: true
+    },
+    data: {
+        type: Object,
+        default:{
+                images : [],
+                iframes : [],
+                label: [],
+                borders: [],
+                likeBtn: [],
+                likeCount: [],
+        }
+    },
+});
+
+module.exports = mongoose.model('dolbyMapSchema', dolbyMapSchema);
